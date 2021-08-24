ee242ef1ae59bd73e4e6770099acf8bd1a2bb131
andgordo <andgordo@ucsc.edu>
2021-08-23 18:45:18 -0700

    Reorganized directory

--- line-set/src/main.py
+++ line-set/src/main.py
@@ -1,3 +1,5 @@
+from . import point_set
+
 
 def retrieve_point_list(is_file: bool, input_name: str = '', points: list = []):
     """
@@ -21,17 +23,23 @@ def retrieve_point_list(is_file: bool, input_name: str = '', points: list = []):
     return points
 
 
+def find_unique_lines(p_set: PointSet):
+    for i in range(p_set.size):
+        j = i + 1
+        p_1 = p_set.points[i]
+        while j < p_set.size:
+            p_2 = p_set.points[j]
+            print(p_set.line_between_points(p_1, p_2))
+            j += 1
+
+
 if __name__ == '__main__':
     # Unit test files must be labeled for text content to be parsed
     unit_test_file = 'unit_tests/test_3_set_3'
     pts = retrieve_point_list(True, unit_test_file, [])
-    print(pts)
 
-    p_s = PointSet(pts)
-    for i in range(p_s.size):
-        j = i + 1
-        p_1 = p_s.points[i]
-        while j < p_s.size:
-            p_2 = p_s.points[j]
-            print(p_s.line_between_points(p_1, p_2))
-            j += 1
+    # Create PointSet object from parsed points list
+    ps = PointSet(pts)
+
+    # Find unique lines between three or more points within point set
+    find_unique_lines(ps)
