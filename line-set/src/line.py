09e2ef76d52688e58316df888a138761cfd34e9e
andgordo <andgordo@ucsc.edu>
2021-08-27 03:02:56 -0700

    Added new graph output, added more robust unit test functions to test_find_lines.py, added float handling for find_unique_lines

--- line-set/src/line.py
+++ line-set/src/line.py
@@ -1,4 +1,4 @@
-from math import gcd
+from math import gcd, floor
 
 
 def convert_lines_to_str(lines: list):
@@ -9,6 +9,30 @@ def convert_lines_to_str(lines: list):
     return [linear_eq_str(line[0], line[1], line[2]) for line in lines]
 
 
+def gcd_int_or_float(a, b):
+    """
+    GCD helper function which allows tolerance to floating point values (math.gcd does not)
+    Code reference from: https://www.geeksforgeeks.org/program-find-gcd-floating-point-numbers/
+
+    :param a: numeric value to find gcd of in relation to b
+    :param b: numeric value to find gcd of in relation to a
+    :return: a gcd for a, b (a and b can be float values)
+    """
+
+    # This check ensures we do not recurse forever when a and b are float representations of whole values (e.g. 1.0)
+    if floor(a) == a and floor(b) == b:
+        return gcd(int(a), int(b))  # Can utilize type intolerant math.gcd as we know a and b are whole values
+
+    if a < b:
+        return gcd_int_or_float(b, a)
+
+    # Base case
+    if abs(b) < 0.001:
+        return a
+    else:
+        return gcd_int_or_float(b, a - floor(a / b) * b)
+
+
 def gcd_abc(a, b, c):
     """
     :param a: number representing a in ax + by = c
@@ -16,7 +40,7 @@ def gcd_abc(a, b, c):
     :param c: number representing c in ax + by = c
     :return: returns gcd of the three values (returns 1 if a,b,c = 0)
     """
-    line_var_gcd = gcd(gcd(a, b), c)
+    line_var_gcd = gcd_int_or_float(gcd_int_or_float(a, b), c)
     if line_var_gcd == 0:
         return 1
     return line_var_gcd
